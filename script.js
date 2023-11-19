24ebeb3e6ae506a0c7e6119a42cf97bc91536ac8
alexandra-svg <150544011+alexandra-svg@users.noreply.github.com>
2023-11-19 20:19:46 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿const codPINCorect = "1234"; // Setează codul PIN dorit
+
+function verificaPIN() {
+    const inputPIN = document.getElementById("pinInput").value;
+    const rezultatDiv = document.getElementById("rezultat");
+
+    if (inputPIN === codPINCorect) {
+        rezultatDiv.innerHTML = "<h2>Cod PIN corect!</h2><img src='imagine.jpg' alt='Imagine cadou'><p>  Cred ca imaginea asta este destul de sugestiva ca sa iti dai seama unde trebuie sa ajungem </p>";
+    } else {
+        rezultatDiv.innerHTML = "<p>Cod PIN incorect. Te rog încearcă din nou.</p>";
+    }
+}
